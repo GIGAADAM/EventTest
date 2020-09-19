2b749500e9fdb35a69e6938c2025055c2aad5b34
GIGAADAM <gigaadam21@gmail.com>
2020-09-19 12:06:36 +0200

    Přidání na GitHub

---
+++ src/main/java/me/gigaadamyt/eventtest/Main.java
@@ -0,0 +1,23 @@
+package me.gigaadamyt.eventtest;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        // Plugin pro GitHub Repository test
+
+
+    }
+
+    @EventHandler
+    public void event(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        p.sendMessage("Pohyb");
+    }
+}
